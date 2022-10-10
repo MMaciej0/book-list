9adeba3f18f3d0436175c02fc346e1abb7ee501c
Maciej Mądry <maciejmadry0@gmail.com>
2022-10-10 20:41:50 +0200

    final commit

---
+++ data.js
@@ -0,0 +1,29 @@
+const books = [
+  {
+    id: 1,
+    title: 'The Institute',
+    author: 'Stephen King',
+    image: 'https://ecsmedia.pl/c/the-institute-b-iext62069348.jpg',
+    year: '1999',
+    category: 'Thriller',
+  },
+  {
+    id: 2,
+    title: 'Hobbit',
+    author: 'J.R.R Tolkien',
+    image:
+      'https://i0.wp.com/static.harpercollins.com/harperimages/isbn/large/8/9780007487288.jpg',
+    year: '1999',
+    category: 'Adventure',
+  },
+  {
+    id: 3,
+    title: 'It',
+    author: 'Stephen King',
+    image: 'https://ecsmedia.pl/c/it-b-iext50542410.jpg',
+    year: '1999',
+    category: 'Horror',
+  },
+];
+
+export default books;
